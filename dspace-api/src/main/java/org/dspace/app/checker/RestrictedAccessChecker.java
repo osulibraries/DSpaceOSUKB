7c4a2a1d8362dafcba55f201be096a385c02109f
Peter Dietz <dietz.72@osu.edu>
2010-12-29 14:08:40 -0500

    Added ability to fix collections/comms to have anonymous read, this is disabled by default, needs command line support still.

--- dspace-api/src/main/java/org/dspace/app/checker/RestrictedAccessChecker.java
+++ dspace-api/src/main/java/org/dspace/app/checker/RestrictedAccessChecker.java
@@ -7,88 +7,113 @@ package org.dspace.app.checker;
 
 import java.sql.SQLException;
 import java.util.Iterator;
+import org.apache.log4j.Logger;
+import org.dspace.authorize.AuthorizeException;
 import org.dspace.authorize.AuthorizeManager;
 import org.dspace.core.Constants;
 import org.dspace.content.Collection;
 import org.dspace.content.Community;
+import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
 import org.dspace.content.ItemIterator;
 import org.dspace.core.Context;
+import org.dspace.eperson.Group;
 
 /**
- *
+ * Command line access to checking comms, colls, items that the anonymous group
+ * can access each one.
  * @author peter
  */
 public class RestrictedAccessChecker {
+    private static final boolean PERFORM_FIX = false;
+    private static final Logger log = Logger.getLogger(RestrictedAccessChecker.class);
+
     private RestrictedAccessChecker()
     {
         ;
     }
-    public static void main(String[] args) throws SQLException
+    public static void main(String[] args) throws SQLException, AuthorizeException
     {
+        //TODO MAKE THIS COMMAND LINE HAPPY
         Context context = new Context();
-        AuthorizeManager auth = new AuthorizeManager();
 
-        //c.setSpecialGroup(0);   // Anonymous group
         RestrictedAccessChecker rac = new RestrictedAccessChecker();
 
         Community[] communities = Community.findAllTop(context);
         for (int com = 0; com < communities.length; com++)
         {
             
-            rac.CheckCommunity(auth, context, communities[com]);
+            rac.CheckCommunity(context, communities[com]);
         }
 
-        ItemIterator items = Item.findAll(context);
+        /*ItemIterator items = Item.findAll(context);
         Item item;
         while(items.hasNext())
         {
             item = items.next();
             rac.CheckItem(auth, context, item);
-        }
+        }*/
 
 
         System.exit(0);
     }
 
-    public void CheckCommunity(AuthorizeManager auth, Context context, Community comm) throws SQLException
+    public void CheckCommunity(Context context, Community comm) throws SQLException, AuthorizeException
     {
         //Check itself
-        if(auth.authorizeActionBoolean(context, comm, Constants.READ) == false)
+        if(AuthorizeManager.authorizeActionBoolean(context, comm, Constants.READ) == false)
         {
             System.out.println("Can't Access Community: " + comm.getHandle() + " -- " + comm.getName());
+            SetAnonymousRead(context, comm);
         }
 
         //check subCols
         Collection[] colls = comm.getCollections();
         for (int col=0; col < colls.length; col++)
         {
-            CheckCollection(auth, context, colls[col]);
+            CheckCollection(context, colls[col]);
         }
 
         //check subComms (recursive)
         Community[] comms = comm.getSubcommunities();
         for (int com=0; com < comms.length; com++)
         {
-            CheckCommunity(auth, context, comms[com]);
+            CheckCommunity(context, comms[com]);
         }
 
     }
 
-    public void CheckCollection(AuthorizeManager auth, Context context, Collection coll) throws SQLException
+    public void CheckCollection(Context context, Collection coll) throws SQLException
     {
-        if(auth.authorizeActionBoolean(context, coll, Constants.READ) == false)
+        if(AuthorizeManager.authorizeActionBoolean(context, coll, Constants.READ) == false)
         {
             System.out.println("Can't Access Collection: " + coll.getHandle() + " -- " + coll.getName());
+            SetAnonymousRead(context, coll);
         }
 
     }
 
-    public void CheckItem(AuthorizeManager auth, Context context, Item item) throws SQLException
+    public void CheckItem(Context context, Item item) throws SQLException
     {
-        if(auth.authorizeActionBoolean(context, item, Constants.READ) == false)
+        if(AuthorizeManager.authorizeActionBoolean(context, item, Constants.READ) == false)
         {
             System.out.println("Can't Access Item: " + item.getHandle() + " -- " + item.getName());
+            SetAnonymousRead(context, item);
+        }
+    }
+
+    public void SetAnonymousRead(Context context, DSpaceObject dso)
+    {
+        try{
+            if(PERFORM_FIX == true)
+            {
+                Group anonymousGroup = Group.find(context, 0);
+                AuthorizeManager.addPolicy(context, dso, Constants.READ, anonymousGroup);
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("ERROR SETTING ANONYMOUS READ");
         }
     }
 
