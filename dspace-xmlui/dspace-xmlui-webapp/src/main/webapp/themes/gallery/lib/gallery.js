73a824f8b42b6460e0e94444a430b2ba23931bea
Peter Dietz <dietz.72@osu.edu>
2011-08-23 11:39:28 -0400

    Reformat JS code in gallery theme. No changes made.

--- dspace-xmlui/dspace-xmlui-webapp/src/main/webapp/themes/gallery/lib/gallery.js
+++ dspace-xmlui/dspace-xmlui-webapp/src/main/webapp/themes/gallery/lib/gallery.js
@@ -28,8 +28,7 @@ var serviceImgUrl = '';
 /**
 * JQuery initialization routine
 */
-$(document).ready(function()
-{
+$(document).ready(function() {
     initZoomableImage();
 
     //$("div.right img").jScale({ls:'200px'});
@@ -41,25 +40,22 @@ $(document).ready(function()
 * set the largest image found as the service image
 * and display it as a zoomable image
 */
-function initZoomableImage()
-{
-    if (imageJpegArray.length >0)
-    {
+function initZoomableImage() {
+    if (imageJpegArray.length > 0) {
         var viewPort = "<div id=\"image_wrap\"><a id=\"anchor\" href=\"#\" class=\"thickbox\"><img class=\"central_image\" alt=\"" + IMAGE_TITLE + "\" src=\"" + THEME_PATH + "images/blank.gif\" /></a></div>";
         var totalHtml = viewPort;
 
         var containHtml = "<div class=\"scrollable\"><div class=\"items\"><div>";
         totalHtml += containHtml;
 
-        for ( var i=0; i<imageJpegArray.length; i++)
-        {
-            totalHtml +=processImage(imageJpegArray[i]);
+        for (var i = 0; i < imageJpegArray.length; i++) {
+            totalHtml += processImage(imageJpegArray[i]);
         }
 
         var tailHtml = "</div></div></div>";
         totalHtml += tailHtml;
         $("#photos").prepend(totalHtml);
-        if(imageJpegArray.length == 1) {
+        if (imageJpegArray.length == 1) {
             $('div.scrollable').css('display', 'none');
 
         }
@@ -72,48 +68,46 @@ function processImage(serviceImg)
 }
 
 
+function initjQueryTools() {
 
-function initjQueryTools () {
+    $(".scrollable").scrollable();
 
-$(".scrollable").scrollable();
+    $(".items img").click(
+        function() {
+            // see if same thumb is being clicked
+            if ($(this).hasClass("active")) {
+                return;
+            }
 
-$(".items img").click(function() {
+            // Currently assuming that thumbnail and full image use same url.
+            var clickedImage = $(this);
+            var url = clickedImage.attr("src");
 
-	// see if same thumb is being clicked
-	if ($(this).hasClass("active")) { return; }
+            // get handle to element that wraps the image and make it semi-transparent
+            var wrap = $("#image_wrap").fadeTo("medium", 0.5);
 
-	// Currently assuming that thumbnail and full image use same url.
-        var clickedImage = $(this);
-	var url = clickedImage.attr("src");
+            var img = new Image();
 
-	// get handle to element that wraps the image and make it semi-transparent
-	var wrap = $("#image_wrap").fadeTo("medium", 0.5);
+            // call this function after it's loaded
+            img.onload = function() {
+                // make wrapper fully visible
+                wrap.fadeTo("fast", 1);
 
-	var img = new Image();
-
-	// call this function after it's loaded
-	img.onload = function() {
-		// make wrapper fully visible
-		wrap.fadeTo("fast", 1);
-
-
-		// change the image
-		wrap.find("img").attr("src", url);
+                // change the image
+                wrap.find("img").attr("src", url);
 
                 //wrap.find('img').jScale({ls:'670px'});
-		$('#anchor').attr("href", url);
-                $('#anchor').attr("title", clickedImage.attr("title") + "<br/>Download: <a href=\""+url+"\">" + clickedImage.attr("alt") + "</a>" );
+                $('#anchor').attr("href", url);
+                $('#anchor').attr("title", clickedImage.attr("title") + "<br/>Download: <a href=\"" + url + "\">" + clickedImage.attr("alt") + "</a>");
+            };
 
+            // begin loading the image
+            img.src = url;
 
-	};
+            // activate item
+            $(".items img").removeClass("active");
+            $(this).addClass("active");
 
-	// begin loading the image
-	img.src = url;
-
-	// activate item
-	$(".items img").removeClass("active");
-	$(this).addClass("active");
-
-// when page loads simulate a "click" on the first image
-}).filter(":first").click();
+        // when page loads simulate a "click" on the first image
+        }).filter(":first").click();
 }
