7b3d30f42911ff29df5e7d46511c5c161e74dc1c
Peter Dietz <dietz.72@osu.edu>
2012-03-12 14:49:53 -0400

    Added faceting for Elastic Search display.
    Also added the date-picker from the Report Generator.

--- dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/dashboard/ElasticSearchStatsViewer.java
+++ dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/dashboard/ElasticSearchStatsViewer.java
@@ -1,18 +1,23 @@
 package org.dspace.app.xmlui.aspect.dashboard;
 
 import edu.osu.library.dspace.statistics.ElasticSearchLogger;
+import org.apache.cocoon.environment.ObjectModelHelper;
 import org.apache.log4j.Logger;
+import org.dspace.app.xmlui.aspect.statistics.ReportGenerator;
 import org.dspace.app.xmlui.cocoon.AbstractDSpaceTransformer;
 import org.dspace.app.xmlui.wing.WingException;
-import org.dspace.app.xmlui.wing.element.Body;
-import org.dspace.app.xmlui.wing.element.Division;
-import org.dspace.app.xmlui.wing.element.PageMeta;
+import org.dspace.app.xmlui.wing.element.*;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.Client;
-import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.facet.FacetBuilders;
+import org.elasticsearch.search.facet.terms.TermsFacet;
+
+import java.util.*;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -32,22 +37,64 @@ public class ElasticSearchStatsViewer extends AbstractDSpaceTransformer {
         Division division = body.addDivision("elastic-stats");
         division.setHead("Elastic Data Display");
         
+        ReportGenerator reportGenerator = new ReportGenerator();
+        reportGenerator.addReportGeneratorForm(division, ObjectModelHelper.getRequest(objectModel));
+        Date dateStart = reportGenerator.getDateStart();
+        Date dateEnd = reportGenerator.getDateEnd();
         
         Client client = ElasticSearchLogger.createElasticClient();
-        
-        QueryBuilder queryBuilder = QueryBuilders.termQuery("city", "columbus");
 
         SearchResponse resp = client.prepareSearch(ElasticSearchLogger.indexName)
                 .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-                .setQuery(queryBuilder)
+                .setQuery(QueryBuilders.matchAllQuery())
+                .addFacet(FacetBuilders.termsFacet("facet1").field("type"))
                 .execute()
                 .actionGet();
 
 
-        division.addPara("Querying bitstreams for elastic, Took "+resp.tookInMillis() + " ms to get "+ resp.hits().totalHits() + " hits.");
+        //division.addPara(resp.toString());
+
+        SearchHits hits = resp.getHits();
+        int numberHits = (int) hits.totalHits();
+
+        division.addPara("Querying bitstreams for elastic, Took " + resp.tookInMillis() + " ms to get " + numberHits + " hits.");
+
+        // Need to cast the facets to a TermsFacet so that we can get things like facet count. I think this is obscure.
+        TermsFacet termsFacet = resp.getFacets().facet(TermsFacet.class, "facet1");
+        List<? extends TermsFacet.Entry> termsFacetEntries = termsFacet.getEntries();
+
+        Table facetTable = division.addTable("facettable", termsFacetEntries.size(), 10);
+        Row facetTableHeaderRow = facetTable.addRow(Row.ROLE_HEADER);
+        facetTableHeaderRow.addCell().addContent("Facet Name");
+        facetTableHeaderRow.addCell().addContent("Count");
         
-        for(SearchHit hit : resp.hits().getHits()) {
-            division.addPara("HIT_ID: " + hit.sourceAsString());
+        for(TermsFacet.Entry facetEntry : termsFacetEntries) {
+            Row row = facetTable.addRow();
+            row.addCell().addContent(facetEntry.getTerm());
+            row.addCell().addContent("" + facetEntry.getCount());
+        }
+                
+        
+        
+        Table table = division.addTable("datatable", numberHits, 18);
+
+        for(SearchHit hit : hits) {
+            Map<String, Object> hitSource = hit.getSource();
+            log.info(hitSource.size());
+            Row thisRow = table.addRow();
+            
+            Set<Map.Entry<String, Object>> setHits = hitSource.entrySet();
+            log.info("NumFields = "+setHits.size());
+                    
+            Iterator fields = setHits.iterator();
+            while(fields.hasNext()) {
+                Map.Entry thisEntry = (Map.Entry) fields.next();
+                Object key = thisEntry.getKey();
+                Object value = thisEntry.getValue();
+                thisRow.addCell().addContent(value.toString());
+                
+            }
+
         }
         
 
@@ -66,4 +113,6 @@ public class ElasticSearchStatsViewer extends AbstractDSpaceTransformer {
 
         
     }
+    
+    
 }
