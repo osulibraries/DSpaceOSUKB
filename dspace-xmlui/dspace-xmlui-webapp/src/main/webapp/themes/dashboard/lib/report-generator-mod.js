77b63392c3c3ceb72f064c39a7f6559151abd42c
Peter Dietz <dietz.72@osu.edu>
2012-05-03 13:50:48 -0400

    Disable the date-picker jqueryui. It needs to show dates in iso8601

--- dspace-xmlui/dspace-xmlui-webapp/src/main/webapp/themes/dashboard/lib/report-generator-mod.js
+++ dspace-xmlui/dspace-xmlui-webapp/src/main/webapp/themes/dashboard/lib/report-generator-mod.js
@@ -1,6 +1,9 @@
 // # ReportGenerator Js Mod
 // ## Addes jQuery functionality to the ReportGenerator
 
-jQuery(function ($) {
-  $(".date-picker").datepicker();
-});
+// Disable the date-picker because it shows mal-formatted dates. we can re-enable when:
+// - it shows dates like 2008-03-24
+// - a user can easily adjust a date manually
+//jQuery(function ($) {
+//  $(".date-picker").datepicker();
+//});
