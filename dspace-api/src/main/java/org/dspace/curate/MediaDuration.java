605de80c79c0c32599e8e94a2eb5e1794adbfe8a
Peter Dietz <dietz.72@osu.edu>
2012-06-21 14:09:49 -0400

    Media Duration curation task can write the duration value into the metadata.

--- dspace-api/src/main/java/org/dspace/curate/MediaDuration.java
+++ dspace-api/src/main/java/org/dspace/curate/MediaDuration.java
@@ -1,5 +1,6 @@
 package org.dspace.curate;
 
+import com.sun.syndication.feed.module.itunes.types.Duration;
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.dspace.content.DCValue;
@@ -42,43 +43,68 @@ public class MediaDuration extends AbstractCurationTask {
             DCValue[] externalMedia = item.getMetadata(externalSourceField);
             //http://somepath.to/media.mp3
             
-            String output = "";
-            
             if(externalMedia != null && externalMedia.length > 0) {
 
-
-
+                //skip doing this step if metadata-field is already filled.
+                DCValue[] formatExtentMetadata = item.getMetadata("dc.format.extent");
+                if(formatExtentMetadata != null && formatExtentMetadata.length > 0) {
+                    curator.setResult(taskId, "Value for dc.format.extent already present.");
+                    return Curator.CURATE_SKIP;
+                }
 
                 URL mediaURL = new URL(externalMedia[0].value);
 
                 File localFile = new File(tempDirectoryPath + "some-downloaded-file");
                 FileUtils.copyURLToFile(mediaURL, localFile);
-
                 
 
-
+                //Todo, generify by accepting config for path to tool.
                 Process mp3InfoProcess = Runtime.getRuntime().exec("/opt/local/bin/mp3info -p \"%S\" " + localFile.getAbsolutePath());
                 BufferedReader reader = new BufferedReader(new InputStreamReader(mp3InfoProcess.getInputStream()));
 
-
-                //might / should only need one line.
+                String resultingValue = "";
                 String line;
-                for(line = reader.readLine(); line != null; line = reader.readLine()) {
-                    output += line;
+                while((line = reader.readLine()) != null) {
+                    resultingValue += line;
                 }
 
-                //stuff the value into the metadata field.
-                //skip doing this step if metadata-field is already filled.
+                try {
+                    resultingValue = resultingValue.trim();
+                    resultingValue = resultingValue.replaceAll("\"", "");
+
+
+                    Integer durationSeconds = Integer.parseInt(resultingValue);
+
+
+                    //stuff the value into the metadata field.
+                    Duration duration = new Duration(0, 0, durationSeconds);
+                    item.addMetadata("dc", "format", "extent", null, "Audio Duration: " + duration.toString());
+                    
+                    try {
+                        item.update();
+                        curator.setResult(taskId, "Set Audio Duration to " + duration.toString());
+                        return Curator.CURATE_SUCCESS;
+
+                    } catch (Exception e) {
+                        curator.setResult(taskId, "Unable to save results");
+                        return Curator.CURATE_ERROR;
+                    }
+
+                } catch (NumberFormatException e) {
+                    curator.setResult(taskId, "Error in processing this media's value. Got:"+resultingValue);
+                    return Curator.CURATE_ERROR;
+                }
+
+
+
                 //fix the output results, to actually display right.
 
-                curator.setResult(taskId, output);
-
-
-                return Curator.CURATE_SUCCESS;
             } else {
+                curator.setResult(taskId, "No media present");
                 return Curator.CURATE_SKIP;
             }
         } else {
+            curator.setResult(taskId, "Not an Item");
             return Curator.CURATE_SKIP;
         }
     }
